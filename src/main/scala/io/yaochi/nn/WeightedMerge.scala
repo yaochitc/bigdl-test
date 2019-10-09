a15a3d56486d66738e4d9e145e9e7627562eb073
yaochi <yaochi@sugo.io>
2019-10-09 11:13:14 +0800

    update the merge layer

--- src/main/scala/io/yaochi/nn/WeightedMerge.scala
+++ src/main/scala/io/yaochi/nn/WeightedMerge.scala
@@ -17,30 +17,47 @@ class WeightedMerge[T: ClassTag](size: Int)
       "3D tensor expected" +
         s"input dimension ${input.nDimension()}")
     output.resize(input.size(1), input.size(3))
-    WeightedMerge.updateOutput[T](input, output)
+    val scales = (1 to size).map(i => ev.exp(weight.valueAt(i)))
+    val scaleSum = scales.reduceLeft((a, b) => ev.plus(a, b))
+
+    val outputArray = output.storage().array()
+    val inputArray = if (input.isContiguous()) {
+      input.storage().array()
+    } else {
+      input.contiguous().storage().array()
+    }
+    val storageOffset = input.storageOffset() - 1
+
     output
   }
 
   override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
+    require(input.size().deep == gradOutput.size().deep,
+      "input should have the same size with gradOutput" +
+        s"inputsize ${input.size().deep} gradOutput ${gradOutput.size().deep}")
+
+    val gradInputArray = gradInput.storage().array()
+    val outputArray = if (output.isContiguous()) {
+      output.storage().array()
+    } else {
+      output.contiguous().storage().array()
+    }
+    val gradOutputArray = if (gradOutput.isContiguous()) {
+      gradOutput.storage().array()
+    } else {
+      gradOutput.contiguous().storage().array()
+    }
+
     gradInput.resizeAs(output)
-    WeightedMerge.updateGradInput[T](input, gradOutput, gradInput, output)
     gradInput
   }
 
 
   override def accGradParameters(input: Tensor[T], gradOutput: Tensor[T]): Unit = {
-  }
-}
+    require(input.size().deep == gradOutput.size().deep,
+      "input should have the same size with gradOutput" +
+        s"inputsize ${input.size().deep} gradOutput ${gradOutput.size().deep}")
 
-object WeightedMerge {
-  private def updateOutput[T: ClassTag](input: Tensor[T], output: Tensor[T])
-                                       (implicit ev: TensorNumeric[T]): Tensor[T] = {
-    null
-  }
 
-  def updateGradInput[T: ClassTag](input: Tensor[T], gradOutput: Tensor[T],
-                                   gradInput: Tensor[T], output: Tensor[T])(implicit ev: TensorNumeric[T]): Tensor[T] = {
-    null
   }
-
 }
