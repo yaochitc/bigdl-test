8c381fafc0e28edc44c5112a242e47a687459760
yaochi <yaochi@sugo.io>
2019-10-09 11:30:20 +0800

    update the merge layer

--- src/main/scala/io/yaochi/nn/WeightedMerge.scala
+++ src/main/scala/io/yaochi/nn/WeightedMerge.scala
@@ -16,9 +16,9 @@ class WeightedMerge[T: ClassTag](size: Int)
     require(input.nDimension() == 3,
       "3D tensor expected" +
         s"input dimension ${input.nDimension()}")
+
     output.resize(input.size(1), input.size(3))
-    val scales = (1 to size).map(i => ev.exp(weight.valueAt(i)))
-    val scaleSum = scales.reduceLeft((a, b) => ev.plus(a, b))
+    val (nFrame, dim, stride) = (input.size(1), input.size(2), input.size(3))
 
     val outputArray = output.storage().array()
     val inputArray = if (input.isContiguous()) {
@@ -28,13 +28,26 @@ class WeightedMerge[T: ClassTag](size: Int)
     }
     val storageOffset = input.storageOffset() - 1
 
+    val scales = (1 to size).map(i => ev.exp(weight.valueAt(i)))
+    val scaleSum = scales.reduceLeft((a, b) => ev.plus(a, b))
+
+    var t = 0
+    while (t < stride * nFrame) {
+      val inputOffset = (t / stride) * dim * stride + t % stride + storageOffset
+      val outputOffset = (t / stride) * dim * stride + t % stride
+
+      var d = 0
+      while (d < dim) {
+        d += 1
+      }
+      t += 1
+    }
+
     output
   }
 
   override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
-    require(input.size().deep == gradOutput.size().deep,
-      "input should have the same size with gradOutput" +
-        s"inputsize ${input.size().deep} gradOutput ${gradOutput.size().deep}")
+    val (nFrame, dim, stride) = (input.size(1), input.size(2), input.size(3))
 
     val gradInputArray = gradInput.storage().array()
     val outputArray = if (output.isContiguous()) {
@@ -48,16 +61,33 @@ class WeightedMerge[T: ClassTag](size: Int)
       gradOutput.contiguous().storage().array()
     }
 
+    var t = 0
+    while (t < stride * nFrame) {
+      var d = 0
+      while (d < dim) {
+        d += 1
+      }
+      t += 1
+    }
+
     gradInput.resizeAs(output)
     gradInput
   }
 
 
   override def accGradParameters(input: Tensor[T], gradOutput: Tensor[T]): Unit = {
-    require(input.size().deep == gradOutput.size().deep,
-      "input should have the same size with gradOutput" +
-        s"inputsize ${input.size().deep} gradOutput ${gradOutput.size().deep}")
+    val (nFrame, dim, stride) = (input.size(1), input.size(2), input.size(3))
 
-
+    val gradInputArray = gradInput.storage().array()
+    val outputArray = if (output.isContiguous()) {
+      output.storage().array()
+    } else {
+      output.contiguous().storage().array()
+    }
+    val gradOutputArray = if (gradOutput.isContiguous()) {
+      gradOutput.storage().array()
+    } else {
+      gradOutput.contiguous().storage().array()
+    }
   }
 }
